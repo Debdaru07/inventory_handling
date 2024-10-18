f28322ea6f074e4bdb02ddce06377fbb8b420274
Debdaru <debdarudasgupta0799.com>
2024-10-18 14:52:58 +0530

    changes in the api get in the front-end.

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,12 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
+export interface DashboardMetrics {
+    
+}
+
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
     tagTypes: [],
     endpoints: (build) => ({
-
-    })
+        getDashboardMetrics: build.query<DashboardMetrics, void>({
+            query: () => "/dashboard",
+            providesTags: []
+        })
+    }),
 })
 
 export const {} = api;
