b1be20ede38c2c43cad011f08f063856e1adfcec
Debdaru <debdarudasgupta0799.com>
2024-10-21 22:32:09 +0530

    add product page creation.

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,22 +1,30 @@
 "use client";
 
-import { useGetProductsQuery } from '@/state/api';
+import { useCreateProductMutation, useGetProductsQuery } from '@/state/api';
 import React, { useState } from 'react'
 import { GridColDef } from '@mui/x-data-grid';
 import Header from '../components/Header';
+import { PlusCircleIcon, SearchIcon } from 'lucide-react';
+import Rating from '../components/Rating';
+import CreateProductModal from './createProductModal';
 
-const columns: GridColDef[] = [
-  {field: "productId", headerName: "ID", width: 90 },
-  {field: "name", headerName: "Product Name", width: 200 },
-  {field: "price", headerName: "Price", width: 110, type: "number",valueGetter: (value, row) => `$${row.price}` },
-  {field: "rating", headerName: "Rating", width: 110, type: "number",valueGetter: (value, row) => row.rating ? row.rating : "N/A" },
-  {field: "stockQuantity", headerName: "Stock Quantity", width: 150, type: "number",},
-];
+type ProductFormData = {
+  name: string;
+  price: number;
+  stockQuantity: number;
+  rating: number;
+};
+
 
 const Products = () => {
   const [searchTerm, setSearchTerm ] = useState("");
   const [isModalOpen, setIsModalOpen ] = useState(false);
+  const [createProduct] = useCreateProductMutation();
   const { data: products, isLoading, isError } = useGetProductsQuery(searchTerm);
+  
+  const handleCreateProduct = async (productData: ProductFormData) => {
+    await createProduct(productData);
+  };
   
   if (isLoading) {
     return <div className='py-4'>Loading ...</div>
@@ -26,16 +34,79 @@ const Products = () => {
     return <div className='text-center text-red-500 py-4'>Failed to fetch Products</div>
   }
 
+  
+
+
   return (
-    <div className='flex flex-col'>
-      <Header name='Products'/>
-      {/* <DataGrid 
-          rows={products} 
-          columns={columns} 
-          getRowId={(row) => row.productId} 
-          checkboxSelection
-          className='bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700'
-        />   */}
+    <div className='mx-auto pb-5 w-full'>
+      {/* SEARCH BAR */}
+      <div className="mb-6">
+        <div className="flex items-center border-2 border-gray-200 rounded">
+          <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
+          <input
+            className="w-full py-2 px-4 rounded bg-white"
+            placeholder="Search products..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      {/* HEADER BAR */}
+      <div className="flex justify-between items-center mb-6">
+        <Header name="Products" />
+        <button
+          className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+          onClick={() => setIsModalOpen(true)}
+        >
+          <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> Create
+          Product
+        </button>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-10 justify-between">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          products?.map((product) => (
+            <div
+              key={product.productId}
+              className="border shadow rounded-md p-4 max-w-full w-full mx-auto"
+            >
+              <div className="flex flex-col items-center">
+                {/* <Image
+                  src={`https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/product${
+                    Math.floor(Math.random() * 3) + 1
+                  }.png`}
+                  alt={product.name}
+                  width={150}
+                  height={150}
+                  className="mb-3 rounded-2xl w-36 h-36"
+                /> */}
+                <h3 className="text-lg text-gray-900 font-semibold">
+                  {product.name}
+                </h3>
+                <p className="text-gray-800">${product.price.toFixed(2)}</p>
+                <div className="text-sm text-gray-600 mt-1">
+                  Stock: {product.stockQuantity}
+                </div>
+                {product.rating && (
+                  <div className="flex items-center mt-2">
+                    <Rating rating={product.rating} />
+                  </div>
+                )}
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+      
+      {/* MODAL */}
+      <CreateProductModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onCreate={handleCreateProduct}
+        // 
+      />
     </div>
   )
 }
